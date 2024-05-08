4e4c9eb34ae3f45f36db413f7fa7a6f12f7bdbf1
Huiju Lee <57425121+leepro1@users.noreply.github.com>
2024-05-08 12:03:37 +0900

    Feat: draw main chart (#10)

--- ssabangFE/src/components/Main/changSection.js
+++ ssabangFE/src/components/Main/changSection.js
@@ -31,7 +31,7 @@ function calculateMonthlyAverage(data, area) {
       while ((year !== currentYear || month !== currentMonth) && (currentYear !== '' || currentMonth !== '')) {
         // 빈 달에 대해 이전 달의 정보를 사용하되, 이전 달이 없으면 기본 가격을 사용
         const average = count === 0 ? 0 : Math.round(sum / count);
-        monthlyAveragePrices[0].push([`${currentYear}-${currentMonth}`, average]);
+        monthlyAveragePrices[0].push([`${currentYear}-${currentMonth}`, parseFloat((average / 10000).toFixed(2))]);
 
         // 다음 달로 이동
         const nextDate = new Date(`${currentYear}-${currentMonth}-01`);
@@ -62,7 +62,7 @@ function calculateMonthlyAverage(data, area) {
   // 오늘의 달까지 데이터를 채웁니다.
   while (currentYear !== todayYear || currentMonth !== todayMonth) {
     const average = count === 0 ? 0 : Math.round(sum / count);
-    monthlyAveragePrices[0].push([`${currentYear}-${currentMonth}`, average]);
+    monthlyAveragePrices[0].push([`${currentYear}-${currentMonth}`, parseFloat((average / 10000).toFixed(2))]);
 
     const nextDate = new Date(`${currentYear}-${currentMonth}-01`);
     nextDate.setMonth(nextDate.getMonth() + 1);
@@ -70,7 +70,6 @@ function calculateMonthlyAverage(data, area) {
     currentMonth = (nextDate.getMonth() + 1).toString().padStart(2, '0');
   }
 
-  monthlyAveragePrices[0].reverse();
   monthlyAveragePrices[1] = chartData.reverse();
   return monthlyAveragePrices;
 }
